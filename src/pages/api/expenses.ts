47cdc7fb294a8787de18ea32aa30ed3758d4ab42
Ariel Plascencia <imariel2d@gmail.com>
2023-05-22 22:29:06 -0700

    Inserting expenses into database

--- src/pages/api/expenses.ts
+++ src/pages/api/expenses.ts
@@ -1,12 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
-type ResponseData = {
-  message: string;
+const prisma = new PrismaClient();
+
+export const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const {
+    title = '',
+    amount = 0,
+    currency = 'MXN'
+  } = req.body;
+
+  const newExpense = await prisma.expense.create({
+    data: {
+      amount,
+      currency,
+      title
+    }
+  });
+
+  const expenses = await prisma.expense.findMany();
+
+  res.status(200).json(expenses);
 };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>,
-) {
-  res.status(200).json({ message: 'Hello from Next.js!' });
-}
+export default handler;

5eb8f1368fd835b43670686366b42e6bbc3848d7
imariel2d <imariel2d@gmail.com>
2023-06-04 11:41:31 -0700

    If user not auth, redirect to login

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,39 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 
-import Box from '@mui/material/Box';
+// ** Third Party Imports
+import { useSession } from 'next-auth/react';
 
+// ** MUI Components
+import { Box } from '@mui/material';
+
+// ** Components Imports
 import { MenuAppBar } from '@/components/navbar';
+import { useRouter } from 'next/router';
 
 interface Props {
   children: ReactNode;
 }
 
 export const Layout: FC<Props> = ({ children }) => {
+  const { data: session, status } = useSession();
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'loading') return; // Wait for session data to load
+
+    if (!session) {
+      // User is not authenticated, redirect to login page
+      router.push('/login');
+    } else {
+      setLoading(false);
+    }
+  }, [session, status, router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <MenuAppBar />
